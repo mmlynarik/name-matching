36bd87af3ea20d7afec590baf87a3de278c57a4f
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2021-10-24 14:51:08 +0200

    Migrate pip -> poetry

--- venv.sh
+++ venv.sh
@@ -1,6 +1,4 @@
 python3.9 -m venv .venv
 echo "set -a && . .env && set +a" >> .venv/bin/activate
 source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-pip install -e .
+poetry install
